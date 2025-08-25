d77238480dedaba00d9b4e1d7b0968c1b701735e
Leon Li <leonlicyuofa@gmail.com>
2025-08-25 15:04:54 +0000

    ok i now got this:
    e: file:///C:/MyCode/udry-umbrella-share-app-v2/andr

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,20 +1,11 @@
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.1" apply false
 }
 
-buildscript {
-    ext {
-        // App dependencies
-        val appCompatVersion by extra("1.6.1")
-        val coreVersion by extra("1.12.0")
-        val junitVersion by extra("4.13.2")
-        val espressoVersion by extra("3.5.1")
-        val testExtJunitVersion by extra("1.1.5")
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
+// Define version variables in a Kotlin-idiomatic way
+val appCompatVersion = "1.6.1"
+val coreVersion = "1.12.0"
+val junitVersion = "4.13.2"
+val espressoVersion = "3.5.1"
+val testExtJunitVersion = "1.1.5"
