b37acb54ec96803287fa223be6306389a3ef48ce
Leon Li <leonlicyuofa@gmail.com>
2025-08-26 14:05:24 +0000

    hmm it's still spinning i copied the xcode log here for your reference:

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.udry.app',
   appName: 'udry 共享雨傘 ',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    hostname: 'udry-app-dev.firebaseapp.com',
+    allowNavigation: [
+      "udry-app-dev.firebaseapp.com",
+      "identitytoolkit.googleapis.com"
+    ],
+  },
+  android: {
+    allowMixedContent: true,
+  },
 };
 
 export default config;
