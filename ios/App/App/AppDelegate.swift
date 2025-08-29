92fda7c6a3704f971847ed53b1cbf97e45f5c046
Leon Li <leonlicyuofa@gmail.com>
2025-08-29 08:22:36 +0000

    i now see the attached in xcode

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,42 +9,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // This is the crucial addition.
+        // We are asking for media capture permissions right after the app launches.
+        // This ensures the WebView has the necessary permissions before any web code tries to access the camera.
+        WKWebView.self.requestMediaCapturePermission(for: .all, granted: { (granted) in
+            if !granted {
+                print("User did not grant media capture permission.")
+            }
+        })
+
         return true
     }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-      return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        // Called when the app was launched with a url. Feel free to add your own implementation.
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-      return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        // Called when the app was launched with an activity, including Universal Links.
+        // Feel free to add your own implementation here.
+        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
-    // This is the updated, backward-compatible function to handle webview permissions for Camera and Bluetooth.
-    func application(_ application: UIApplication, didCreate newSceneSession: UISceneSession, for connectionOptions: UIScene.ConnectionOptions, with bridge: CAPBridgeProtocol) {
-        if let webView = bridge.webView {
-            // This is a legacy method, but it's the most reliable way to ask the webview
-            // to grant permissions for media capture (camera/microphone) on our specific app origin.
-            // This is the key line that enables the native permission pop-up to be triggered by our web code.
-            WKWebsiteDataStore.default().requestMediaCapturePermission(for: webView.configuration.websiteDataStore, origin: webView.url!, initiatedByFrame: nil) { (granted) in
-                if granted {
-                    print("Media capture permission granted to webview.")
-                } else {
-                    print("Media capture permission denied to webview.")
-                }
-            }
-        }
+    override func remoteControlReceived(with event: UIEvent?) {
+        super.remoteControlReceived(with: event)
     }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
 }
