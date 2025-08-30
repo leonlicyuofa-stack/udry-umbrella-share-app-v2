f8c97007a0bc526f0a306b4a86ebbb9a51fc8bf2
Leon Li <leonlicyuofa@gmail.com>
2025-08-30 18:03:18 +0000

    ok i got the following
    u-dry-app @0.1.0 build
    > next build
    
       ▲ Next.j

--- src/app/payment/cancel/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being moved to /src/app/(external)/payment/cancel/page.tsx and will be deleted from here.
-// The presence of this empty file content in the plan indicates its deletion from this path.
--- src/app/payment/success/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being moved to /src/app/(external)/payment/success/page.tsx and will be deleted from here.
-// The presence of this empty file content in the plan indicates its deletion from this path.
