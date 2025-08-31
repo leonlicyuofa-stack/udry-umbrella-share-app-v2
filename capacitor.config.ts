357052fd3210c61f60fb54afc78e760569494496
Leon Li <leonlicyuofa@gmail.com>
2025-08-31 09:43:11 +0000

    i now got this again
    leonli@Lis-MacBook-Air udry-umbrella-share-app-v2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.udry.app',
   appName: 'udry 共享雨傘 ',
-  webDir: '.next',
+  webDir: 'out',
   android: {
     allowMixedContent: true,
   },
