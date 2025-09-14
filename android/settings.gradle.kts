8813cd49203116d76fe1d62631467bc82cc7b2b6
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 09:06:58 +0000

    ok i now got the follwong, what is it telling us?
    leonli@Lis-MacBook-Ai

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,6 +1,31 @@
-// THIS IS A GENERATED FILE, DO NOT EDIT
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android"
 include(":app")
 include(":capacitor-android")
-project(":capacitor-android").projectDir = file("capacitor-android")
+include(":capacitor-app")
+include(":capacitor-camera")
+include(":capacitor-community-bluetooth-le")
 include(":capacitor-community-sqlite")
-project(":capacitor-community-sqlite").projectDir = file("capacitor-community-sqlite")
+include(":capacitor-cordova-android-plugins")
+include(":capacitor-status-bar")
+
+project(":capacitor-android").projectDir = file("../node_modules/@capacitor/android/capacitor")
+project(":capacitor-app").projectDir = file("../node_modules/@capacitor/app/android")
+project(":capacitor-camera").projectDir = file("../node_modules/@capacitor/camera/android")
+project(":capacitor-community-bluetooth-le").projectDir = file("../node_modules/@capacitor-community/bluetooth-le/android")
+project(":capacitor-community-sqlite").projectDir = file("../node_modules/@capacitor-community/sqlite/android")
+project(":capacitor-status-bar").projectDir = file("../node_modules/@capacitor/status-bar/android")
