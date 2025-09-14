08bd535cfdc6a60b18806a7d6607b17e080928df
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 09:13:29 +0000

    ok let's go ahdea

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.android.gradlePlugin)
+        classpath(libs.kotlin.gradlePlugin)
+        classpath(libs.google.services.plugin)
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.googleServices) apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
