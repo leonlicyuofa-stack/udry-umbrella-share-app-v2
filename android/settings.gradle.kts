b20459d9409520e13a5901937d52acb2e3eb7eb2
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 09:08:35 +0000

    ok please go ahead

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 dependencyResolutionManagement {
@@ -22,10 +22,3 @@ include(":capacitor-community-bluetooth-le")
 include(":capacitor-community-sqlite")
 include(":capacitor-cordova-android-plugins")
 include(":capacitor-status-bar")
-
-project(":capacitor-android").projectDir = file("../node_modules/@capacitor/android/capacitor")
-project(":capacitor-app").projectDir = file("../node_modules/@capacitor/app/android")
-project(":capacitor-camera").projectDir = file("../node_modules/@capacitor/camera/android")
-project(":capacitor-community-bluetooth-le").projectDir = file("../node_modules/@capacitor-community/bluetooth-le/android")
-project(":capacitor-community-sqlite").projectDir = file("../node_modules/@capacitor-community/sqlite/android")
-project(":capacitor-status-bar").projectDir = file("../node_modules/@capacitor/status-bar/android")
