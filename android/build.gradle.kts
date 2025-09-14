525e61b3303a8deeb0e5ba188a932adedf76f0e0
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 09:28:42 +0000

    ok i think you made the cahnge to the wrong fodler again, our root folde

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.googleServices) apply false
+}
+
 buildscript {
     repositories {
         google()
@@ -6,21 +12,7 @@ buildscript {
     }
     dependencies {
         classpath(libs.android.gradlePlugin)
-        classpath(libs.kotlin.gradlePlugin)
         classpath(libs.google.services.plugin)
-    }
-}
-
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.googleServices) apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -12,13 +12,19 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "android"
 include(":app")
 include(":capacitor-android")
+project(":capacitor-android").projectDir = file("../node_modules/@capacitor/android/capacitor")
 include(":capacitor-app")
+project(":capacitor-app").projectDir = file("../node_modules/@capacitor/app/android")
 include(":capacitor-camera")
+project(":capacitor-camera").projectDir = file("../node_modules/@capacitor/camera/android")
 include(":capacitor-community-bluetooth-le")
+project(":capacitor-community-bluetooth-le").projectDir = file("../node_modules/@capacitor-community/bluetooth-le/android")
 include(":capacitor-community-sqlite")
+project(":capacitor-community-sqlite").projectDir = file("../node_modules/@capacitor-community/sqlite/android")
 include(":capacitor-cordova-android-plugins")
+project(":capacitor-cordova-android-plugins").projectDir = file("capacitor-cordova-android-plugins")
 include(":capacitor-status-bar")
+project(":capacitor-status-bar").projectDir = file("../node_modules/@capacitor/status-bar/android")
