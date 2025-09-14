2f2754880f40f04bd6655758344470e162d0dcc3
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 15:04:15 +0000

    ok let's do it

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,37 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    // We apply the plugins here at the root level.
-    // The versions are now managed centrally in the project's version catalog (`libs.versions.toml`).
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.googleServices) apply false
+
+// Apply the allprojects block to configure all projects in the build
+allprojects {
+    // Repositories for all projects
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// Configure sub-projects (including Capacitor plugins)
+subprojects {
+    // This block applies configurations after each sub-project has been evaluated.
+    afterEvaluate {
+        // Check if the sub-project has the 'android' extension (i.e., is an Android module)
+        if (project.extensions.findByName("android") is com.android.build.gradle.api.AndroidBasePlugin) {
+            // Apply common Android settings
+            project.extensions.configure<com.android.build.gradle.api.AndroidBasePlugin>("android") {
+                val compileSdkVersion: String by project
+                val buildToolsVersion: String by project
+
+                compileSdkVersion(compileSdkVersion)
+                buildToolsVersion(buildToolsVersion)
+
+                // Common lint options for all Android modules
+                lint {
+                    abortOnError = false
+                }
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
