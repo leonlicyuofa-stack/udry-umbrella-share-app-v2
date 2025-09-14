c6cb43cd6239c1458092692b1a9c4e70afef2917
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 14:56:32 +0000

    ok let's try it

---
+++ Users/leonli/Desktop/udry-umbrella-share-app-v2/android/settings.gradle.kts
@@ -0,0 +1,37 @@
+
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android"
+include(":app")
+include(":capacitor-android")
+include(":capacitor-app")
+include(":capacitor-camera")
+include(":capacitor-community-bluetooth-le")
+include(":capacitor-community-sqlite")
+include(":capacitor-cordova-android-plugins")
+include(":capacitor-status-bar")
+project(":capacitor-android").projectDir = file("../node_modules/@capacitor/android/capacitor")
+project(":capacitor-app").projectDir = file("../node_modules/@capacitor/app/android")
+project(":capacitor-camera").projectDir = file("../node_modules/@capacitor/camera/android")
+project(":capacitor-community-bluetooth-le").projectDir = file("../node_modules/@capacitor-community/bluetooth-le/android")
+project(":capacitor-community-sqlite").projectDir = file("../node_modules/@capacitor-community/sqlite/android")
+project(":capacitor-cordova-android-plugins").projectDir = file("capacitor-cordova-android-plugins")
+project(":capacitor-status-bar").projectDir = file("../node_modules/@capacitor/status-bar/android")
+
+    
