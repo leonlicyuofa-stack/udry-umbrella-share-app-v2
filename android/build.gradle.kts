ee98141178f95fbd4c5e1f2a8c19006686b324f8
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 17:42:44 +0000

    ok please go ahead

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.googleServices) apply false
 }
 
-// Apply common configurations to all Android projects (app and libraries)
 subprojects {
-    afterEvaluate { project ->
-        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
-            project.extensions.findByType(com.android.build.api.dsl.CommonExtension::class.java)?.apply {
-                // Read the compileSdkVersion from the root project's properties
-                val compileSdkVersion: String by rootProject
-                compileSdk = compileSdkVersion.toInt()
-            }
+    afterEvaluate {
+        android {
+            val compileSdkVersion: String by project
+            compileSdkVersion(compileSdkVersion.toInt())
         }
     }
 }
