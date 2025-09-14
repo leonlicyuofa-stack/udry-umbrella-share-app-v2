af72dbc605b885e03cc96b844a5e8df543085a31
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 15:11:21 +0000

    ok please go ahead

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,49 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.googleServices) apply false
 }
 
-// Define project-wide versions as extra properties
-val compileSdkVersion by extra(34)
-val buildToolsVersion by extra("34.0.0")
-val minSdkVersion by extra(22)
-val targetSdkVersion by extra(34)
-val kotlinVersion by extra("1.9.22")
-
-// Apply repositories to all projects (root and sub-projects)
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-// Apply Android-specific configurations ONLY to sub-projects (like :app and capacitor plugins)
-subprojects {
-    // Check if the sub-project has an Android plugin before trying to configure it
-    plugins.withId("com.android.base") {
-        extensions.configure<com.android.build.gradle.api.AndroidBasePlugin> {
-            compileSdkVersion(34)
-            buildToolsVersion("34.0.0")
-
-            defaultConfig {
-                minSdk = 22
-                targetSdk = 34
-                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-            }
-
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-    }
-}
-
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+// Define versions in a central place to be used by sub-projects
+ext {
+    set("compileSdkVersion", 34)
+    set("minSdkVersion", 22)
+    set("targetSdkVersion", 34)
+    set("buildToolsVersion", "34.0.0")
 }
