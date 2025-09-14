88ebada1dbca753246e416587b0816507787b228
Leon Li <leonlicyuofa@gmail.com>
2025-09-14 17:45:02 +0000

    ok lets do it

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,15 +2,20 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.googleServices) apply false
 }
 
 subprojects {
-    afterEvaluate {
-        android {
-            val compileSdkVersion: String by project
-            compileSdkVersion(compileSdkVersion.toInt())
+    val compileSdkVersion by project.extra(providers.gradleProperty("compileSdkVersion"))
+
+    plugins.withId("com.android.application") {
+        extensions.getByType<com.android.build.api.dsl.ApplicationExtension>().apply {
+            compileSdk = compileSdkVersion.toInt()
+        }
+    }
+    plugins.withId("com.android.library") {
+        extensions.getByType<com.android.build.api.dsl.LibraryExtension>().apply {
+            compileSdk = compileSdkVersion.toInt()
         }
     }
 }
