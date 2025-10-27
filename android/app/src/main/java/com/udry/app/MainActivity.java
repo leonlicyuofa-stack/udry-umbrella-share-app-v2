580f4603ce348e87017832fb799b70a0fab76a8a
Leon Li <leonlicyuofa@gmail.com>
2025-10-27 14:27:46 +0000

    ok pelase go ahead

--- android/app/src/main/java/com/udry/app/MainActivity.java
+++ android/app/src/main/java/com/udry/app/MainActivity.java
@@ -1,48 +1,6 @@
 
 package com.udry.app;
 
-import android.os.Bundle;
-import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-  private static final String TAG = "UDRY_DIAGNOSTIC";
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    Log.d(TAG, "--- MainActivity.onCreate() START ---");
-    super.onCreate(savedInstanceState); 
-    Log.d(TAG, "--- MainActivity.onCreate() (super.onCreate() finished) ---");
-  }
-
-  @Override
-  public void onStart() {
-      super.onStart();
-      Log.d(TAG, "--- MainActivity.onStart() --- The activity is becoming visible to the user.");
-  }
-
-  @Override
-  public void onResume() {
-      super.onResume();
-      Log.d(TAG, "--- MainActivity.onResume() --- The activity has come to the foreground.");
-  }
-
-  @Override
-  public void onPause() {
-      super.onPause();
-      Log.d(TAG, "--- MainActivity.onPause() --- The activity is going into the background.");
-  }
-
-  @Override
-  public void onStop() {
-      super.onStop();
-      Log.d(TAG, "--- MainActivity.onStop() --- The activity is no longer visible.");
-  }
-
-  @Override
-  public void onDestroy() {
-      super.onDestroy();
-      Log.d(TAG, "--- MainActivity.onDestroy() --- The activity is being destroyed.");
-  }
-}
+public class MainActivity extends BridgeActivity {}
