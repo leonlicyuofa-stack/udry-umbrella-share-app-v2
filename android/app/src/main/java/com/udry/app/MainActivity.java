0bbaeb9c821ad86cd0528a0aa6a0a97fb220eb30
Leon Li <leonlicyuofa@gmail.com>
2025-10-27 16:33:33 +0000

    ok please go ahead

--- android/app/src/main/java/com/udry/app/MainActivity.java
+++ android/app/src/main/java/com/udry/app/MainActivity.java
@@ -1,22 +1,6 @@
 
 package com.udry.app;
 
-import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // This is the crucial bug fix.
-        // It disables hardware acceleration for the WebView to work around a
-        // low-level graphics driver issue in the Android Emulator. This prevents the "white screen" crash.
-        WebView webView = getBridge().getWebView();
-        if (webView != null) {
-            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
